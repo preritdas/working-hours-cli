0f9b323d4e48889fc3ba8d75be5b857c8004ef48
Prerit Das <preritdas@gmai.com>
2022-07-23 01:16:18 -0400

    Bitly module for link shortening.

---
+++ bitly.py
@@ -0,0 +1,18 @@
+import requests
+import _keys
+
+
+def _bitly(long_link: str) -> str:
+    """Shortens link using the bitly API."""
+    if not isinstance(long_link, str):
+        raise Exception("You must provide a string.")
+
+    headers = {
+        'Authorization': f'Bearer {_keys.Bitly.access_token}',
+        'Content-Type': 'application/json'
+    }
+
+    data = ' {"long_url": "' + long_link + '" } '
+    
+    response = requests.post(url="https://api-ssl.bitly.com/v4/shorten", headers=headers, data=data)
+    return response.json()['link']
