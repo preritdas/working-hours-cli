f5c600fcd8ac2d5d83e6ff96cc4247e67871b1d4
Prerit Das <preritdas@gmail.com>
2022-08-04 11:56:55 -0400

    Place for RapidAPI key.

--- _keys (sample).py
+++ _keys (sample).py
@@ -1,6 +1,20 @@
 class Deta:
+    """
+    Where all your monthly log databases are stored.
+    """
     project_key: str = ''
 
+
 class Bitly:
+    """
+    Shorten long links in PDF reports.
+    """
     access_token: str = ''
+    
+
+class RapidAPI:
+    """
+    Smart string capitalization, only if enabled config.ini.
+    """
+    api_key: str = ''
     
